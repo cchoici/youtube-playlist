6a51bb2fd47ad69516639fc58a292f1ffc6cffeb
eric.lee <eric.hh.lee@deltaww.com>
2018-08-06 01:22:58 +0800

    fix: store

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import { configureStore, history } from './store/configureStore';
 import App from './App';
 import './app.global.scss';
 
+const store = configureStore();
+
 ReactDom.render(
   <AppContainer>
-    <App />
+    <App store={store} history={history} />
   </AppContainer>,
   document.getElementById('app')
 );
@@ -16,7 +19,7 @@ if (module.hot) {
     const NextRoot = require('./App'); // eslint-disable-line global-require
     ReactDom.render(
       <AppContainer>
-        <NextRoot />
+        <NextRoot store={store} history={history} />
       </AppContainer>,
       document.getElementById('app')
     );
---
+++ app/store/configureStore.prod.js
@@ -0,0 +1,18 @@
+// @flow
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { createHashHistory } from 'history';
+import { routerMiddleware } from 'react-router-redux';
+import rootReducer from '../reducers';
+// import type { counterStateType } from '../reducers/counter';
+
+const history = createHashHistory();
+const router = routerMiddleware(history);
+const enhancer = applyMiddleware(thunk, router);
+
+// function configureStore(initialState?: counterStateType) {
+function configureStore(initialState) {
+  return createStore(rootReducer, initialState, enhancer);
+}
+
+export default { configureStore, history };
